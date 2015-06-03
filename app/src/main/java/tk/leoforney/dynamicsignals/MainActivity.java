825f25c975789c06f8d27c06a8339c708516e11d
Leo Forney <forneyleo@gmail.com>
2015-06-02 20:45:46 -0500

    Remove ActionBar

--- app/src/main/java/tk/leoforney/dynamicsignals/MainActivity.java
+++ app/src/main/java/tk/leoforney/dynamicsignals/MainActivity.java
@@ -1,6 +1,7 @@
 package tk.leoforney.dynamicsignals;
 
 import android.content.Context;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -15,6 +16,9 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
 
         // Dynamic Signals URL
         String dsurl = "http://7351ftc.tumblr.com";
